026c48f000f41a09e0eece979f37e639ae8e09ff
Stephen Battista <stephen.battista@gmail.com>
2019-08-05 18:55:00 -0400

    first commit

---
+++ src/main.rs
@@ -0,0 +1,23 @@
+use std::str;
+use data_encoding::BASE64;
+fn main() {
+    let mut buffer =[0;1024];
+    let input = b"SGVsbG8gd29ybGQ=";
+    let output = &mut buffer[0 .. BASE64.decode_len(input.len()).unwrap()];
+    let _len = BASE64.decode_mut(input, output).unwrap();
+    let s =  str::from_utf8(input).unwrap();
+    println!("{:?}",s);
+    let s = str::from_utf8(output).unwrap();
+    println!("{:?}",s);
+
+    let mut buffer =[0;1024];
+    let input2 = output;
+    let output2 = &mut buffer[0 .. BASE64.encode_len(input2.len())];
+    let _len = BASE64.encode_mut(input2, output2);
+    let s = str::from_utf8(input2).unwrap();
+    println!("{:?}",s);
+    let s = str::from_utf8(output2).unwrap();
+    println!("{:?}",s);
+
+
+}
