a83bb51dc5cff5ca40d85f04b6c50aa779639fa6
Stephen Battista <stephen.battista@gmail.com>
2021-11-17 17:02:37 -0500

    Test completed with Tokio

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,50 @@
-use std::str;
 use data_encoding::BASE64;
-fn main() {
-    let mut buffer =[0;1024];
-       let input = b"SGVsbG8gd29ybGQ=";
-       let output = &mut buffer[0 .. BASE64.decode_len(input.len()).unwrap()];
-       let len = BASE64.decode_mut(input, output).unwrap();
-       let output = &mut buffer[0 .. len];
-       let s0 =  str::from_utf8(input).unwrap().to_string();
-       let s1 = str::from_utf8(output).unwrap().to_string();
-       println!("\"{}\" decodes to \"{}\"",s0,s1);
-       
+use rand::{distributions::Standard, thread_rng, Rng};
+static NUMBER_OF_RUNS: usize = 20000000;
+const MAX_U8_LEN: usize = 512;
+const MAX_NUMBER_OF_UUENCODE_PER_U8: usize = 4;
+static HOW_OFTEN_TO_REPORT: usize = 10000;
+fn remove_right_zeros(input: &[u8]) -> &[u8] {
+    let mut local = input;
+    loop {
+        if local.is_empty() {
+            break;
+        } else if *local.last().unwrap() == 0 {
+            local = &local[..local.len() - 1];
+        } else {
+            break;
+        }
+    }
+    local
+}
 
-       let mut buffer =[0;1024];
-       let input2 = output;
-       let output2 = &mut buffer[0 .. BASE64.encode_len(input2.len())];
-       BASE64.encode_mut(input2, output2);
-       let s2 = str::from_utf8(input2).unwrap().to_string();
-       let s3 = str::from_utf8(output2).unwrap().to_string();
-       println!("\"{}\" decodes to \"{}\"",s2,s3);
-
-       assert_eq!(s0,s3);
-       assert_eq!(s1,s2);
-
-
+fn does_uuencode_match_input(input: &[u8]) -> bool {
+    let mut buffer = [0; MAX_U8_LEN * MAX_NUMBER_OF_UUENCODE_PER_U8];
+    let output = &mut buffer[0..BASE64.encode_len(input.len())];
+    BASE64.encode_mut(input, output);
+    let mut buffer = [0; MAX_U8_LEN + 1];
+    let output2 = &mut buffer[0..BASE64.decode_len(output.len()).unwrap()];
+    BASE64.decode_mut(output, output2).unwrap();
+    let output2 = remove_right_zeros(output2);
+    let input = remove_right_zeros(input);
+    input == output2
+}
+#[tokio::main]
+async fn main() {
+   
+    let mut rng = rand::thread_rng();
+    for run_number in 0..NUMBER_OF_RUNS {
+        let input : Vec<u8> = thread_rng()
+            .sample_iter(Standard)
+            .take(rng.gen_range(0..MAX_U8_LEN))
+            .collect::<Vec<u8>>();
+            tokio::spawn(async move {
+                 if !does_uuencode_match_input(&input) {
+                    println!("{:?} won't convert", &input);
+                }});
+        
+        if run_number % HOW_OFTEN_TO_REPORT == 0 {
+            println!("Testing run number {}.", run_number)
+        }
+    }
 }
